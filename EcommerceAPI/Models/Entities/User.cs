f8ab04a0948b6fc628da51973a58b4fbd0d330f7
JordyMorenoArias <yordimorenoarias.11@gmail.com>
2025-04-09 14:12:04 -0400

    A migration was created where properties (Provider, ProviderId) were added to the User entity for the OAuth implementation.

--- EcommerceAPI/Models/Entities/User.cs
+++ EcommerceAPI/Models/Entities/User.cs
@@ -9,10 +9,10 @@ namespace EcommerceAPI.Models.Entities
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "FirstName is required")]
+        [Required(ErrorMessage = "FirstName is required"), MaxLength(100)]
         public string FirstName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "LastName is required")]
+        [Required(ErrorMessage = "LastName is required"), MaxLength(100)]
         public string? LastName { get; set; }
 
         [MaxLength(100)]
@@ -21,16 +21,21 @@ namespace EcommerceAPI.Models.Entities
         [Required(ErrorMessage = "Email is required"), EmailAddress, MaxLength(100)]
         public string Email { get; set; } = string.Empty;
 
-        [Required, PasswordPropertyText]
-        public string PasswordHash { get; set; } = string.Empty;
+        [PasswordPropertyText]
+        public string? PasswordHash { get; set; } = null;
 
         public UserRole Role { get; set; } = UserRole.Customer;
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+        public UserProvider Provider { get; set; } = UserProvider.local;
+
+        [MaxLength(200)]
+        public string? ProviderId { get; set; } = null;
+
         public bool IsEmailConfirmed { get; set; } = false;
 
-        [MaxLength(100)]
+        [MaxLength(200)]
         public string? EmailConfirmedToken { get; set; }
         public int? ResetPasswordCode { get; set; }
         public DateTime? ResetTokenExpiresAt { get; set; }
